7b8a430f1a8ae7f0d43baabb19bf93ac2531779a
John Melville <JohnMelville@gmail.com>
2023-12-30 06:41:51 -0500

    Properties  for build task

--- Src/Melville.HeatForWix/HarvestAssembly.cs
+++ Src/Melville.HeatForWix/HarvestAssembly.cs
@@ -1,12 +1,18 @@
-﻿using Microsoft.Build.Utilities;
+﻿using Microsoft.Build.Framework;
+using Microsoft.Build.Utilities;
 
 namespace Melville.HeatForWix;
 
 public class HarvestAssembly: Task
 {
+    [Required]
+    public string Source { get; set; }
+    [Required]
+    public string Destination { get; set; }
+    
     public override bool Execute()
     {
-        Log.LogWarning("John Melville's custom build task is running.");
+        Log.LogWarning($"From [{Source}] to [{Destination}]");
         return true;
     }
 }
